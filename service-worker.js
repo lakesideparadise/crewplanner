0506dbbcbcf0688efe37458f0c2169568e9ee11c
lakesideparadise <nick@lakesideparadise.be>
2026-08-05 12:48:32 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,31 @@
-/* Compatibility loader. CableCrew uses firebase-messaging-sw.js from v111 onward. */
-importScripts("./firebase-messaging-sw.js?v=111");
+const CACHE="cablecrew-v112";
+const CORE=["./","./index.html","./manifest.webmanifest","./cablecrew_logo.png","./icon-192.png","./icon-512.png"];
+
+self.addEventListener("install",event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE).catch(()=>{})));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate",event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch",event=>{
+  const request=event.request;
+  if(request.method!=="GET")return;
+  const url=new URL(request.url);
+  if(url.origin!==self.location.origin)return;
+  if(request.mode==="navigate"){
+    event.respondWith(fetch(request).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE).then(cache=>cache.put("./index.html",copy)).catch(()=>{});
+      return response;
+    }).catch(()=>caches.match("./index.html")));
+    return;
+  }
+  event.respondWith(fetch(request).then(response=>{
+    if(response&&response.ok){const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(request,copy)).catch(()=>{});}
+    return response;
+  }).catch(()=>caches.match(request)));
+});
